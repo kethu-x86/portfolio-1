f1946b526a7ec38fac139b391fd6fb982de21b6e
Anukheth Sunil M <anukhethsunil@gmail.com>
2026-02-22 11:46:10 +0530

    lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Lenis from ' lenis'
+import Lenis from 'lenis'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
